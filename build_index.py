06595219c43b1ba58a8c0e525dc3628d90a3b07f
bldr1862 <bldr1862@gmail.com>
2019-11-24 21:55:09 -0300

    final

--- build_index.py
+++ build_index.py
@@ -8,7 +8,7 @@ import os
 from sklearn.neighbors import BallTree
 #from scipy.spatial.distance import cosine
 
-ficheros = utils.getFiles(params.VLAD_PCA_DESCRIPTORS_VAL_PATH)
+ficheros = utils.getFiles(params.CNN_PCA_DESCRIPTORS_TEST_PATH)
 arrays = utils.loadNumpyArrays(ficheros)
 n_arrays = []
 for array in arrays:
@@ -19,10 +19,10 @@ order = dict(zip(range(len(ficheros)), ficheros))
 
 tree = BallTree(n_arrays,leaf_size=40, metric='euclidean')
 
-with open(params.VLAD_VAL_INDEX,'wb') as handle:
+with open(params.CNN_TEST_INDEX,'wb') as handle:
     pickle.dump(tree, handle, 2) 
 
-with open(params.VLAD_VAL_ORDER,'wb') as handle:
+with open(params.CNN_TEST_ORDER,'wb') as handle:
     pickle.dump(order, handle, 2) 
 
 print('INDEX AND ORDER SAVED')
